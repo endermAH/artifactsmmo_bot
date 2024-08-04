721bdd55e0ed6b4b3c8223b3307b557c08283fbe
Evgeny Kuryatov <evgeny.kuryatov@netcracker.com>
2024-08-04 21:37:53 +0500

    Add data template

---
+++ data_template.py
@@ -0,0 +1,12 @@
+# Game data
+API_URL = "https://api.artifactsmmo.com"
+TOKEN="****"
+HEADERS = {
+    "Accept": "application/json",
+    "Content-Type": "application/json",
+    "Authorization": f"Bearer {TOKEN}"
+}
+
+# Telegramm data
+TG_BOT_KEY = "****"
+CHAT_ID = "****"
